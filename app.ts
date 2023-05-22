0921117dc4c463d543813bf06a968b9c2163725a
Muhammad Shuaib Khalid <81217682+meShuaibKhalid@users.noreply.github.com>
2023-05-22 18:44:35 +0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -4,6 +4,9 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
+app.use(express.json());
+app.use(urlencoded({ limit: '50mb', extended: false }))
+
 app.use("/api/v1/users", require('./api/user'));
 app.use("/api/v1/hydration", require('./api/hydration'));
 
